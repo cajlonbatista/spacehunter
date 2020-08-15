34c0705de440f3858d5e9486acda0dc605dc9ca3
Francisco Cajlon Jhonathan Moura Batista <63024734+cajlonbatista@users.noreply.github.com>
2020-08-15 20:10:49 -0300

    Update index.js

--- src/pages/News/index.js
+++ src/pages/News/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { message } from 'antd';
+import { message, Spin } from 'antd';
 import { SolarSystemLoading } from 'react-loadingg';
 
 import { Loading } from '../../styles/general';
@@ -12,27 +12,51 @@ export default class News extends Component {
     super();
     this.state = {
       articles: [],
+      total: 0,
+      page: 1,
       loading: true,
+      spinnig: false,
     };
   }
 
   componentDidMount() {
-    news.get('/articles')
-      .then(({ data: { docs: articles } }) => this.setState({ articles, loading: false }))
+    news.get(`/articles/${this.state.page}`)
+      .then(({ data: { docs: articles, page: page } }) => this.setState({ articles, loading: false, page: this.state.page + 1,total: page }))
       .catch(() => {
         message.error('Algo deu errado tente mais tarde!');
       });
   }
+  loaderArticles = () => {
+    if(this.state.page >= this.state.total){
 
+    }else{
+    news.get(`/articles/${this.state.page}`)
+      .then(({ data: { docs: articles } }) => {
+          console.log(articles);
+      })
+      .catch(() => {
+        message.error('Algo deu errado tente mais tarde!');
+      });
+    }
+    console.log(this.state.total)
+  }
   render() {
     const { articles, loading } = this.state;
 
     return loading
       ? <Loading><SolarSystemLoading /></Loading>
       : (
-        <NewsGrid>
-          { articles.map((article) => <CardNews news={article} />) }
-        </NewsGrid>
+        <Spin spinning={this.state.spinnig}>
+          <NewsGrid>
+            {articles.map((article) => <CardNews news={article} />)}
+          </NewsGrid>
+          <div>
+            <a href="#" onClick={this.loaderArticles}>
+              Carregar mais
+            </a>
+          </div>
+        </Spin>
+
       );
   }
 }
