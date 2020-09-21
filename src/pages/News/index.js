45529a90f1575ce7349c1b268b91ca10fb895ea2
cajlonbatista <cajlonbatista@outlook.com.br>
2020-09-21 12:33:18 -0300

    Add components

--- src/pages/News/index.js
+++ src/pages/News/index.js
@@ -6,6 +6,7 @@ import { Loading } from '../../utils/styles/general';
 import { NewsGrid, Container } from './styles';
 import CardNews from '../../components/CardNews';
 import news from '../../utils/news';
+import Pagination from '@material-ui/lab/Pagination';
 
 const style = {
   height: 40,
@@ -48,31 +49,6 @@ export default class News extends Component {
           });
       })
   }
-  loaderArticles = () => {
-    if (this.state.page < 1) {
-      this.setState({
-        enable: true,
-      })
-    } else {
-      this.setState({
-        spinning: true
-      })
-      news.get(`/articles?page=${this.state.page}`)
-        .then(({ data: { docs: articles } }) => {
-          for (const i of articles) {
-            const item = this.state.articles.push(i);
-            this.setState({
-              article: item,
-              spinning: false,
-              page: this.state.page - 1
-            });
-          }
-        })
-        .catch(() => {
-          message.error('Algo deu errado tente mais tarde!');
-        });
-    }
-  }
   render() {
     const { articles, loading } = this.state;
 
@@ -88,9 +64,7 @@ export default class News extends Component {
               {articles.map((article) => <CardNews key={article._id} news={article} />)}
             </NewsGrid>
             <Container>
-              <Button block disabled={this.state.enable} style={{ margin: "0 auto" }} onClick={this.loaderArticles}>
-                More
-            </Button>
+            <Pagination count={10} page={page} onChange={handleChange} />
             </Container>
           </div>
           
