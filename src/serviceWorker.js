a493c5b4aa6c67e6226e1944b5d7a100a4306cf1
Francisco Cajlon Jhonathan Moura Batista <cajlonbatista@outlook.com.br>
2020-12-10 19:11:39 -0300

    Finished to pwa

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,7 +1,7 @@
 export const registerServiceWorker = () => {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-      navigator.serviceWorker.register('/serviceWorker.js').then(function (registration) {
+      navigator.serviceWorker.register('sw.js').then(function (registration) {
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }).catch(function (err) {
         console.log('ServiceWorker registration failed: ', err);
