8cf8283a2097bbe14d0739dd4fac8a9653e15d37
Francisco Cajlon Jhonathan Moura Batista <cajlonbatista@outlook.com.br>
2020-12-10 19:40:17 -0300

    pwa ok

--- public/sw.js
+++ public/sw.js
@@ -12,6 +12,7 @@ var urlsToCache = [
 ];
 
 self.addEventListener('install', function (event) {
+  // Perform install steps
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function (cache) {
@@ -24,6 +25,7 @@ self.addEventListener('fetch', function (event) {
   event.respondWith(
     caches.match(event.request)
       .then(function (response) {
+        // Cache hit - return response
         if (response) {
           return response;
         }
