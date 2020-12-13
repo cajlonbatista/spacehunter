bd516f5e300e82b97654c9b019e84ce23ed609a7
Francisco Cajlon Jhonathan Moura Batista <cajlonbatista@outlook.com.br>
2020-12-13 20:30:42 -0300

    finished planets grid e dialog info

--- src/pages/Universe/System/styles.js
+++ src/pages/Universe/System/styles.js
@@ -18,12 +18,14 @@ export const PageHeader = styled.div`
     font-size: 25px;
   }
   @media(max-width: 500px){
+    padding-top: 90px;
+    padding-bottom: 20px;
     flex-direction: column;
     svg{
-      width: 40px;
+      width: 55px;
     }
     h2{
-      font-size: 15px;
+      font-size: 19px;
     }
   }
 `;
@@ -39,25 +41,98 @@ export const HeaderTitle = styled.h2`
   color: #faad14;
   font-size: 20px;
 `;
-
-export const DrawerInfo = styled.div`
-  margin: 20px auto;
-  border: 1px solid #faad14;
-  border-radius: 5px;
+export const DialogContent = styled.main`
+  width: 100vw;
+  height: 100vh;
+  background-color: #000000;
   padding: 10px;
-
-  strong { color: #faad14; }
-`;
-
-export const DrawerButton = styled.button`
-  background-color: #faad14;
-  color: #181818;
-  padding: 10px 20px;
-  border-radius: 5px;
-  transition: all 0.5s;
-
-  &:hover {
-    opacity: 0.9;
-    color: white;
+  overflow: auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  justify-content: center;
+  position: relative;
+  @media(max-width: 700px){
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+  >figure{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    img{
+      width: 100%;
+      max-width: 600px;
+      object-fit: cover;
+      border-radius: 10px;
+    }
+    figcaption{
+      font-family: Nunito, sans-serif;
+      font-size: 14px;
+      color: #FFFFE9;
+      width: 100%;
+      max-width: 600px;
+      text-align: center;
+    }  
+  }
+  >button:first-child{
+    position: absolute;
+    top: 20px;
+    left: 50%;
+    svg{
+      width: 24px;
+      height: 24px;
+    }  
+  }
+  >section{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    >article{
+      width: 100%;
+      max-width: 400px;
+      margin: 20px auto;
+      display: flex;
+      flex-direction: column;
+      border: 2px solid #D07E03;
+      border-radius: 5px;
+      padding: 10px;
+      font-family: Nunito, sans-serif;
+      color: #FFFFE9;
+      div{
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+      }
+      strong {
+        color: #D07E03; 
+      }
+    }
+    >a{
+      background-color: #D07E03;
+      display: block;
+      margin: 0 auto;
+      padding: 10px 20px;
+      border-radius: 5px;
+      font-family: Nunito, sans-serif;
+      border: 2px solid transparent;
+      font-size: 14px;
+      width: 100%;
+      max-width: 400px;
+      color: #000;
+      font-weight: 600;
+      text-transform: uppercase;
+      text-align: center;
+      :hover, :focus{
+        color: #000;
+      }
+      @media(max-width: 500px){
+        padding: 5px 10px;
+        font-size: 13px;
+      } 
+    }
   }
 `;
